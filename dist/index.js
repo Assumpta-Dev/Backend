309fb57ef570390d0f1101bc51f1aab9e74eb284
Assumpta-tech <uwamariya.assumpta@yahoo.com>
2026-01-27 09:09:04 +0200

    fixing json files

--- dist/index.js
+++ dist/index.js
@@ -5,17 +5,19 @@ import express from "express";
 import categoriesRoutes from "./routes/categories";
 import productsRoutes from "./routes/products";
 import authenticationRouter from "./routes/auth";
+import usersRouter from "./routes/users";
 import connectdb from "./database.connect";
 dotenv.config();
 const PORT = process.env.PORT || 6000;
 const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
-app.use("/auth", authRoutes);
 //routes
 app.use("/category", categoriesRoutes);
 app.use("/product", productsRoutes);
 app.use("/auth", authenticationRouter);
+app.use("/auth", authRoutes);
+app.use("/users", usersRouter);
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to the API" });
 });
