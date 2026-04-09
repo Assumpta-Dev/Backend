76e03af9fff594a62623cc0d230571b461339a11
Assumpta-tech <uwamariya.assumpta@yahoo.com>
2026-04-09 20:43:58 +0200

    fix: parse PORT env var as number

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ dotenv.config();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const PORT = process.env.PORT || 7000;
+const PORT = Number(process.env.PORT) || 7000;
 const app = express();
 
 app.use(morgan("dev"));
